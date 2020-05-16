8119a067d49b263266225097c048037e5f7c136b
Adrian Matejov <ado.matejov@gmail.com>
2020-05-16 22:56:34 +0200

    typo: inference graph model name
    Signed-off-by: Adrian Matejov <ado.matejov@gmail.com>

--- scripts/inference.py
+++ scripts/inference.py
@@ -7,8 +7,8 @@ y2 = [0.04193, 0.13818, 0.24460, 0.36614, 0.57220, 0.72777]
 y3 = [0.08024, 0.24169, 0.37760, 0.54790, 0.87124, 1.11313]
 y4 = [0.10610, 0.36903, 0.60164, 0.89698, 1.42379, 1.83285]
 
-plt.plot(x, y1, label='ResNet 4')
-plt.plot(x, y2, label='SegNet 4')
+plt.plot(x, y1, label='ResNet-4')
+plt.plot(x, y2, label='SegNet-4')
 plt.plot(x, y3, label='ResNet')
 plt.plot(x, y4, label='SegNet')
 plt.ylabel('Time in sec')
